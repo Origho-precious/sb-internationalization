56e4e736051c44d99c72915255d415a6b2f52e2d
Origho-precious <Origho9@gmail.com>
2023-05-01 16:15:15 +0100

    feat: adding Layout

---
+++ src/components/composed/Layout/Layout.tsx
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import styled from "styled-components";
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
+interface Props {
+	children: React.ReactNode;
+	noBodyWrapper?: boolean;
+}
+
+const Layout: React.FC<Props> = ({ children, noBodyWrapper }) => {
+	return (
+		<StyledLayout className="bg-blue">
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin=""
+				/>
+			</Head>
+			<main className="transition-all duration-500 ease-in-out">
+				<div>
+					<Navbar />
+					{!noBodyWrapper ? (
+						<div className="fk-content custom-container mx-auto">
+							{children}
+						</div>
+					) : (
+						<div className="fk-content">{children}</div>
+					)}
+					<Footer />
+				</div>
+			</main>
+		</StyledLayout>
+	);
+};
+
+const StyledLayout = styled.article`
+	min-height: 100vh;
+
+	& > main {
+		& > div {
+			max-width: 1440px;
+			margin: 0 auto;
+			flex-direction: column;
+			justify-content: space-between;
+
+			& > .fk-content {
+				width: 100%;
+				margin: auto;
+				min-height: 78vh;
+			}
+		}
+	}
+`;
+
+export default Layout;
