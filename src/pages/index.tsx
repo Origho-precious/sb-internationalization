be69e32696b90a9e2f77383ae08f90f3116c1ee8
Origho-precious <Origho9@gmail.com>
2023-05-05 21:37:05 +0100

    feat: adding features section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,33 @@ const customers = [
 	},
 ];
 
+const features = [
+	{
+		iconUrl: "/images/icons/idea.svg",
+		title: "Idea Management",
+		description:
+			"Capture and organize your ideas in one place, and collaborate with your team to refine them into actionable plans.",
+	},
+	{
+		iconUrl: "/images/icons/roadmap.svg",
+		title: "Roadmap Planning",
+		description:
+			"Visualize your product development timeline and prioritize tasks to ensure you stay on track.",
+	},
+	{
+		iconUrl: "/images/icons/project.svg",
+		title: "Project Management",
+		description:
+			"Assign tasks, set deadlines, and track progress in real-time to keep everyone on the same page.",
+	},
+	{
+		iconUrl: "/images/icons/feedback.svg",
+		title: "Feedback Collection",
+		description:
+			"Gather feedback from stakeholders and customers to ensure your product meets their needs.",
+	},
+];
+
 const Homepage = () => {
 	return (
 		<Layout>
@@ -95,6 +122,34 @@ const Homepage = () => {
 						</Marquee>
 					</div>
 				</section>
+
+				<section className="sb-features mx-auto">
+					<header className="mx-auto">
+						<h1 className="text-center text-secondary font-inter">
+							Features <span className="text-primary">uniquely</span> tailored
+							for you
+						</h1>
+						<p className="text-center">
+							Our tool is designed to be flexible and customizable to fit your
+							unique needs. Whether you're a startup looking to launch your
+							first product or a large enterprise managing a portfolio of
+							products, our platform can help you stay organized and focused on
+							your goals.
+						</p>
+					</header>
+
+					<div className="flex flex-col items-center justify-center">
+						<div className="grid lg:grid-cols-2 grid-cols-1 gap-[74px] w-full">
+							{features.map((feature) => (
+								<FeatureCard>
+									<img src={feature.iconUrl} alt="feature-icon" />
+									<h3 className="font-inter text-secondary">{feature.title}</h3>
+									<p>{feature.description}</p>
+								</FeatureCard>
+							))}
+						</div>
+					</div>
+				</section>
 			</Wrapper>
 		</Layout>
 	);
@@ -127,6 +182,7 @@ const Wrapper = styled.div`
 
 		& .sb-para-1 {
 			margin-top: 25px;
+			line-height: 168.52%;
 
 			@media screen and (max-width: 529px) {
 				margin-top: 16px;
@@ -198,7 +254,7 @@ const Wrapper = styled.div`
 		left: 0;
 		right: 0;
 		position: absolute;
-		margin-bottom: 65px;
+		height: 225px;
 		padding: 61px 0 60px;
 		background: rgba(0, 179, 176, 0.12);
 
@@ -218,6 +274,98 @@ const Wrapper = styled.div`
 				}
 			}
 		}
+	}
+
+	& .sb-features {
+		margin-top: 430px;
+
+		& > header {
+			max-width: 734px;
+
+			@media screen and (max-width: 539px) {
+				width: 100%;
+			}
+
+			& h1 {
+				font-weight: 800;
+				font-size: 64px;
+				line-height: 77px;
+				margin-bottom: 26px;
+
+				@media screen and (max-width: 639px) {
+					font-size: 48px;
+					line-height: 60px;
+				}
+
+				@media screen and (max-width: 500px) {
+					font-size: 36px;
+					line-height: 50px;
+					margin-bottom: 16px;
+				}
+			}
+
+			& > p {
+				font-weight: 400;
+				font-size: 20px;
+				line-height: 168.5%;
+				margin-bottom: 86px;
+
+				@media screen and (max-width: 639px) {
+					margin-top: 16px;
+				}
+			}
+		}
+
+		& > div {
+			@media screen and (max-width: 1023px) {
+				gri
+			}
+		}
+	}
+`;
+
+const FeatureCard = styled.div`
+	height: max-content;
+	max-width: 551px;
+	background: #ffffff;
+	border-radius: 16px;
+	padding: 59px 57px 72px;
+	border: 2px solid #00b3b0;
+	box-shadow: 9px 9px 0px #00b3b0;
+
+	@media screen and (max-width: 1023px) {
+		padding: 40px 30px 50px;
+		margin: 0 auto;
+	}
+
+	@media screen and (max-width: 639px) {
+		padding: 24px 24px;
+		margin: 0 auto;
+	}
+
+	&:nth-child(even) {
+		margin-top: 24px;
+
+		@media screen and (max-width: 1023px) {
+			margin-top: 0;
+		}
+	}
+
+	& > img {
+		width: 64px;
+		height: 64px;
+	}
+
+	& > h3 {
+		font-size: 20px;
+		font-weight: 700;
+		margin: 9px 0 12px;
+		line-height: 168.5%;
+	}
+
+	& > p {
+		font-size: 20px;
+		line-height: 168.5%;
 	}
 `;
 
