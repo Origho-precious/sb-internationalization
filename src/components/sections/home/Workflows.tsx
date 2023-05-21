2f2440fa2335420c98dcb8e90a11e40d1ab315ab
Origho-precious <Origho9@gmail.com>
2023-05-21 13:12:29 +0100

    feat: decoupling homepage

---
+++ src/components/sections/home/Workflows.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Workflow = () => {
+	return <Section>Workflow</Section>;
+};
+
+const Section = styled.section``;
+
+export default Workflow;
