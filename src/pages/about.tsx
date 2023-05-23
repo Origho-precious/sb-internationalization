fbb06a4811c440da15456b3a868d6846677ee14c
Origho-precious <Origho9@gmail.com>
2023-05-23 22:51:36 +0100

    feat: adding responsive intro section for about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,20 @@
+import Layout from "../components/composed/Layout/Layout";
+import Intro from "../components/sections/about/Intro";
+import Customers from "../components/sections/home/Customers";
+import Features from "../components/sections/home/Features";
+import Testimonials from "../components/sections/home/Testimonials";
+import Pricing from "../components/sections/home/Pricing";
+import Workflows from "../components/sections/home/Workflows";
+import Supercharge from "../components/sections/home/Supercharge";
+
+const Aboutpage = () => {
+	return (
+		<Layout>
+			<>
+				<Intro />
+			</>
+		</Layout>
+	);
+};
+
+export default Aboutpage;
