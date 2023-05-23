9669e6df44d06d8682c7019b2a3b9bbfb53dd717
Origho-precious <Origho9@gmail.com>
2023-05-23 23:11:08 +0100

    feat: adding about page Companies section

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,13 @@
 import Layout from "../components/composed/Layout/Layout";
 import Intro from "../components/sections/about/Intro";
-import Customers from "../components/sections/home/Customers";
-import Features from "../components/sections/home/Features";
-import Testimonials from "../components/sections/home/Testimonials";
-import Pricing from "../components/sections/home/Pricing";
-import Workflows from "../components/sections/home/Workflows";
-import Supercharge from "../components/sections/home/Supercharge";
+import Companies from "../components/sections/about/Companies";
 
 const Aboutpage = () => {
 	return (
 		<Layout>
 			<>
-				<Intro />
+        <Intro />
+        <Companies />
 			</>
 		</Layout>
 	);
