97a2b635eead6d61c3f2e05b4124c7c9745ef86b
Origho-precious <Origho9@gmail.com>
2023-05-24 21:10:51 +0100

    feat: adding CoreValues section to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,7 @@ import Companies from "../components/sections/about/Companies";
 import TeamAndCulture from "../components/sections/about/TeamAndCulture";
 import Supercharge from "../components/sections/home/Supercharge";
 import Platform from "../components/sections/about/Platform";
+import CoreValues from "../components/sections/about/CoreValues";
 
 const Aboutpage = () => {
 	return (
@@ -11,8 +12,9 @@ const Aboutpage = () => {
 			<>
 				<Intro />
 				<Companies />
-        <TeamAndCulture />
-        <Platform />
+				<TeamAndCulture />
+				<CoreValues />
+				<Platform />
 				<Supercharge />
 			</>
 		</Layout>
