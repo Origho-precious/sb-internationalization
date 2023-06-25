36eeafeee7bd4cbc2b37a60c4567b3d0cdd6f1f1
Origho-precious <Origho9@gmail.com>
2023-06-25 12:17:36 +0100

    feat: updating homepage to use storyblok API

---
+++ src/interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface Blok {
+	blok: any;
+}
+
+export interface StoryblokImage {
+	filename: string;
+	alt: string;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,43 @@
+import {
+	useStoryblokState,
+	getStoryblokApi,
+	StoryblokComponent,
+	ISbStoryParams,
+} from "@storyblok/react";
 import Layout from "../components/composed/Layout/Layout";
-import Intro from "../components/sections/home/Intro";
-import Customers from "../components/sections/home/Customers";
-import Features from "../components/sections/home/Features";
-import Testimonials from "../components/sections/home/Testimonials";
-import Pricing from "../components/sections/home/Pricing";
-import Workflows from "../components/sections/home/Workflows";
-import Supercharge from "../components/sections/home/Supercharge";
 
-const Homepage = () => {
+const Homepage = ({ story }: any) => {
+	story = useStoryblokState(story);
+
 	return (
-		<Layout>
-			<>
-				<Intro />
-				<Customers />
-				<Features />
-				<Testimonials />
-				<Pricing />
-				<Workflows />
-				<Supercharge />
-			</>
-		</Layout>
+		<>
+			<Layout>
+				<div className="md:mt-20 mt-8">
+					<StoryblokComponent blok={story.content} />
+				</div>
+			</Layout>
+		</>
 	);
 };
+
+export async function getStaticProps() {
+	const slug = "home";
+
+	const sbParams: ISbStoryParams = {
+		version: "draft", // or 'published'
+	};
+
+	const storyblokApi = getStoryblokApi();
+
+	const res = await storyblokApi?.get(`cdn/stories/${slug}`, sbParams);
+
+	return {
+		props: {
+			story: res?.data ? res?.data?.story : false,
+			key: res?.data ? res?.data?.story.id : false,
+		},
+		revalidate: 3600,
+	};
+}
 
 export default Homepage;
