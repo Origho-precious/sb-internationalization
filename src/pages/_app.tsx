3bb861be0258f4fa1cb9fddc8b6f3e49ab53b572
Origho-precious <Origho9@gmail.com>
2023-06-25 12:03:22 +0100

    feat: storyblok config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Customers from "../components/sections/home/Customers";
+import Features from "../components/sections/home/Features";
+import Intro from "../components/sections/home/Intro";
+import Page from "../components/composed/Page";
+import Supercharge from "../components/sections/home/Supercharge";
+import Testimonials from "../components/sections/home/Testimonials";
+import Workflows from "../components/sections/home/Workflows";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	storyblokInit({
+		accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
+		use: [apiPlugin],
+		components: {
+			Customers,
+			Features,
+			Intro,
+			page: Page,
+			Supercharge,
+			Testimonial: Testimonials,
+			Workflows,
+		},
+	});
+
+	return <Component {...pageProps} />;
 }
