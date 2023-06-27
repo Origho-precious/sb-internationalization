5e69b291ecf353f997adfd9bab5ebf4a120eec1a
Origho-precious <Origho9@gmail.com>
2023-06-27 23:10:43 +0100

    fix: deployment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { storyblokInit, apiPlugin } from "@storyblok/react";
 import Page from "../components/composed/Page";
+import Perk from "../components/atoms/Perk/Perk";
 import Intro from "../components/sections/home/Intro";
 import Pricing from "../components/sections/home/Pricing";
 import Features from "../components/sections/home/Features";
@@ -10,7 +11,6 @@ import Customers from "../components/sections/home/Customers";
 import Supercharge from "../components/sections/home/Supercharge";
 import Testimonials from "../components/sections/home/Testimonials";
 import PricingCard from "../components/composed/PricingCard/PricingCard";
-import Perk from "../components/atoms/Perk/Perk";
 
 export default function App({ Component, pageProps }: AppProps) {
 	storyblokInit({
